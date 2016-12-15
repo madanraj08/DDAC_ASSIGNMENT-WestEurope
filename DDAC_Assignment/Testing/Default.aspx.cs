b0a2acc7b2e39e86154d5eb3ec695a69b2c4bd18
madanraj08 <madan.r08@gmail.com>
2016-12-15 11:32:03 +0800

    West Europe Update

---
+++ DDAC_Assignment/Testing/Default.aspx.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace Testing
+{
+    public partial class _Default : Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void SearchButton_Click(object sender, EventArgs e)
+        {
+            Response.Redirect("Contact.aspx");
+            Server.Transfer("Contact.aspx");
+           
+        }
+    }
+}
